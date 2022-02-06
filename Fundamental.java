2af4ff1cd6b6c6205a6bce6ed8310f0fc394be89
giannicastillo <66094112+giannicastillo@users.noreply.github.com>
2022-02-06 14:48:04 -0500

    First Java programming assignment completed !

---
+++ Fundamental.java
@@ -0,0 +1,8 @@
+
+public class Fundamental{
+    public static void main(String[] args){
+        System.out.println("Hello my name is Gianni Castillo.");
+        System.out.println("I am 25 years old.");
+        System.out.println("My hometown is NYC!");
+    }
+}
